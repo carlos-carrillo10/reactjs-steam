6a0eb85975eeb876f31c4cf82dfb36873a851ae6
Carlos Carrillo <rodrigo0095@hotmail.com>
2022-01-02 15:31:26 -0600

    change class to className on Loader code

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
 export const Loader = () =>
 {
-    return  <div class="flex items-center justify-center ">
-                <div class="w-10 h-10 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    return  <div className="flex items-center justify-center ">
+                <div className="w-10 h-10 border-b-2 border-gray-900 rounded-full animate-spin"></div>
             </div>
 }
